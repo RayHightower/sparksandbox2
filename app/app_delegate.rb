30979f10afd5ce7a5d395f1e2b5d8eeee15595a5
Raymond T. Hightower <rth@wisdomgroup.com>
2013-05-10 21:45:25 -0500

    write initial app_delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,13 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = HomeController.new
+    @window.makeKeyAndVisible
+
+    @window.backgroundColor = UIColor.blueColor
+
+    @thing1 = 5
+    
     true
   end
 end
